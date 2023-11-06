05664b755e9758650734b249e5978f1bb98ba402
shape-warrior-t <bl.swt1@gmail.com>
2023-11-06 18:21:54 -0500

    Add definitions files

---
+++ src/astra/data/alarms.py
@@ -0,0 +1,93 @@
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
+from typing import NewType
+
+from parameters import ParameterValue, Tag
+
+
+@dataclass
+class EventBase:
+    persistence: float | None
+    description: str
+
+
+@dataclass
+class RateOfChangeEventBase(EventBase):
+    tag: Tag
+    rate_of_fall_threshold: float | None
+    rate_of_rise_threshold: float | None
+    time_window: float
+
+
+@dataclass
+class StaticEventBase(EventBase):
+    tag: Tag
+
+
+@dataclass
+class ThresholdEventBase(EventBase):
+    tag: Tag
+    lower_threshold: float | None
+    upper_threshold: float | None
+
+
+@dataclass
+class SetpointEventBase(EventBase):
+    tag: Tag
+    setpoint: ParameterValue
+
+
+@dataclass
+class SOEEventBase(EventBase):
+    event_bases: list[EventBase]
+    intervals: list[tuple[float, float | None]]
+
+
+@dataclass
+class AllEventBase(EventBase):
+    event_bases: list[EventBase]
+
+
+@dataclass
+class AnyEventBase(EventBase):
+    event_bases: list[EventBase]
+
+
+@dataclass
+class NotEventBase(EventBase):
+    event_base: EventBase
+
+
+EventID = NewType('EventID', int)
+
+
+@dataclass
+class Event:
+    base: EventBase
+    id: EventID
+    time: datetime
+    description: str
+
+
+class AlarmCriticality(Enum):
+    WARNING = 'WARNING'
+    LOW = 'LOW'
+    MEDIUM = 'MEDIUM'
+    HIGH = 'HIGH'
+    CRITICAL = 'CRITICAL'
+
+
+type AlarmPriority = AlarmCriticality
+
+
+@dataclass
+class AlarmBase:
+    event_base: EventBase
+    criticality: AlarmCriticality
+
+
+@dataclass
+class Alarm:
+    event: Event
+    criticality: AlarmCriticality
