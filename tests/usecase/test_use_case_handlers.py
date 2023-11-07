7834c95d1aa71398c62426d5ce4fb1e022199ccb
alexma22 <alex.mathioudakis@mail.utoronto.ca>
2023-11-07 04:14:03 -0500

    Updated parametrzing method in test cases.

--- tests/usecase/test_use_case_handlers.py
+++ tests/usecase/test_use_case_handlers.py
@@ -4,72 +4,54 @@ from astra.data.data_manager import (
     DataManager,
     telemetry_0,
     telemetry_1,
-    config
 )
 
 from astra.usecase.dashboard_handler import (
     TableReturn,
-    DashboardHandler,
-    Tag,
+    DashboardHandler
 )
 
+# Creating a mock data for the test cases.
 MOCKTELEMETRY0 = 'telemetry0.h5'
+MOCKTABLELIST0 = [
+    ['A3', 'a monad', '5 m', '3'],
+    ['B1', 'is a monoid', '0.0 g', '1.0'],
+    ['B4', 'in the category', 'True', 'False'],
+    ['C1', 'of endofuntors', '2 s', 'None']
+]
+MOCKTABLE0 = TableReturn(
+    telemetry_0.get('EPOCH')[0],
+    MOCKTABLELIST0,
+    [],
+    3
+)
+
+# Creating the mock telemetry data frame for the second file.
 MOCKTELEMETRY1 = 'telemetry1.h5'
+MOCKTABLELIST1 = [
+    ['A3', 'a monad', '5 m', '3'],
+    ['B1', 'is a monoid', '3000.0 g', '1.0'],
+    ['B4', 'in the category', 'False', 'False'],
+    ['C1', 'of endofuntors', '1 s', 'None']
+]
+MOCKTABLE1 = TableReturn(
+    telemetry_1.get('EPOCH')[0],
+    MOCKTABLELIST1,
+    [],
+    2
+)
 DEVICE = 'DEVICE'
 
 
-@pytest.fixture
-def data0():
-    datamanager = DataManager.from_device_name(DEVICE)
-    start_time = datamanager.add_data_from_file(MOCKTELEMETRY0)
-
-    # Uses data from telemetry0.h5
-    telemetry_table = [
-        ['A3', 'a monad', '5 m', '3'],
-        ['B1', 'is a monoid', '0.0 g', '1.0'],
-        ['B4', 'in the category', 'True', 'False'],
-        ['C1', 'of endofuntors', '2 s', 'None']
-    ]
-
-    table = TableReturn(
-        telemetry_0.get('EPOCH')[0],
-        telemetry_table,
-        [],
-        3
-    )
-
-    return datamanager, start_time, table
-
-
-@pytest.fixture
-def data1():
-    datamanager = DataManager.from_device_name(DEVICE)
-    start_time = datamanager.add_data_from_file(MOCKTELEMETRY1)
-
-    # Uses data from telemetry1.h5
-    telemetry_table = [
-        ['A3', 'a monad', '5 m', '3'],
-        ['B1', 'is a monoid', '3000.0 g', '1.0'],
-        ['B4', 'in the category', 'False', 'False'],
-        ['C1', 'of endofuntors', '1 s', 'None']
-    ]
-
-    table = TableReturn(
-        telemetry_1.get('EPOCH')[0],
-        telemetry_table,
-        [],
-        2
-    )
-
-    return datamanager, start_time, table
-
-
-@pytest.mark.parametrize('data,start_time,tablereturn', [data0, data1])
-def test_dashboard_no_filter(data: DataManager, start_time: datetime, table: TableReturn):
+@pytest.mark.parametrize('telemetry_file, tablereturn', [(MOCKTELEMETRY0, MOCKTABLE0),
+                                                         (MOCKTELEMETRY1, MOCKTABLE1)])
+def test_dashboard_no_filter(telemetry_file: str, tablereturn: TableReturn):
     """
     A test case for the dashboard use case handler with no filters.
     We expect the full table to be returned.
     """
+    data = DataManager.from_device_name(DEVICE)
+    start_time = data.add_data_from_file(telemetry_file)
 
     # creates a datatable and adds data to it and retrieves the data.
     DashboardHandler.set_index(0)
@@ -78,19 +60,23 @@ def test_dashboard_no_filter(data: DataManager, start_time: datetime, table: Tab
     DashboardHandler.set_end_time(None)
     actual = DashboardHandler.get_data(data)
 
-    expected = table
+    expected = tablereturn
 
     assert (
         actual == expected
     )
 
 
-@pytest.mark.parametrize('data,start_time,tablereturn', [data0, data1])
-def test_dashboard_one_filter(data: DataManager, start_time: datetime, table: TableReturn):
+@pytest.mark.parametrize('telemetry_file, tablereturn', [(MOCKTELEMETRY0, MOCKTABLE0),
+                                                         (MOCKTELEMETRY1, MOCKTABLE1)])
+def test_dashboard_one_filter(telemetry_file: str, tablereturn: TableReturn):
     """
     A test case for the dashboard use case handler with one filter.
     We expect a table with one column removed be returned.
     """
+
+    data = DataManager.from_device_name(DEVICE)
+    start_time = data.add_data_from_file(telemetry_file)
 
     # creates a datatable and adds data to it and retrieves the data.
     DashboardHandler.set_index(0)
@@ -103,20 +89,24 @@ def test_dashboard_one_filter(data: DataManager, start_time: datetime, table: Ta
 
     actual = DashboardHandler.get_data(data)
 
-    expected = table
-    expected.table = table.table[1:]
+    expected = tablereturn
+    expected.table = tablereturn.table[1:]
 
     assert (
         actual == expected
     )
 
 
-@pytest.mark.parametrize('data,start_time,tablereturn', [data0, data1])
-def test_dashboard_all_filters(data: DataManager, start_time: datetime, table: TableReturn):
+@pytest.mark.parametrize('telemetry_file, tablereturn', [(MOCKTELEMETRY0, MOCKTABLE0),
+                                                         (MOCKTELEMETRY1, MOCKTABLE1)])
+def test_dashboard_all_filters(telemetry_file: str, tablereturn: TableReturn):
     """
     A test case for the dashboard use case handler with every tag filtered.
     We expect an empty list to be returned.
     """
+
+    data = DataManager.from_device_name(DEVICE)
+    start_time = data.add_data_from_file(telemetry_file)
 
     # creates a datatable and adds data to it and retrieves the data.
     DashboardHandler.set_index(0)
@@ -132,8 +122,8 @@ def test_dashboard_all_filters(data: DataManager, start_time: datetime, table: T
 
     actual = DashboardHandler.get_data(data)
 
-    expected = table
-    expected.table = table.table[len(table.table):]
+    expected = tablereturn
+    expected.table = tablereturn.table[len(tablereturn.table):]
 
     assert (
         actual == expected
