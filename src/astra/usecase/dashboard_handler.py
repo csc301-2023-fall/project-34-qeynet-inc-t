20a0af1b8f8af54374ca8564d0bb0884319ec7df
Liam <liam.odero1@gmail.com>
2023-11-08 09:52:51 -0500

    performed method extraction in a number of areas

--- src/astra/usecase/dashboard_handler.py
+++ src/astra/usecase/dashboard_handler.py
@@ -2,8 +2,9 @@ from dataclasses import dataclass
 from datetime import datetime
 from .use_case_handlers import UseCaseHandler
 from astra.data.data_manager import DataManager
-from astra.data.telemetry_data import TelemetryData
+from astra.data.telemetry_data import TelemetryData, TelemetryFrame
 from astra.data.parameters import Parameter, ParameterValue
+from .utils import eval_param_value, get_tag_param_value, get_tag_params
 
 SORT = 'SORT'
 TAG = 'TAG'
@@ -129,26 +130,8 @@ class DashboardHandler(UseCaseHandler):
         cls.end_time = end_time
 
     @classmethod
-    def _eval_param_value(cls, tag_parameter: Parameter,
-                          tag_data: ParameterValue) -> float | int | bool | None:
-        """
-        Converts the raw <parameter_data> into its true value using the
-        parameter multiplier and constant
-
-        :param tag_parameter: Parameter data for the relevant tag
-        :param tag_data: The raw data in the telemetry frame
-        :return: The converted parameter value
-        """
-        if type(tag_data) is bool or tag_data is None:
-            return tag_data
-        else:
-            multiplier = tag_parameter.display_units.multiplier
-            constant = tag_parameter.display_units.constant
-            return tag_data * multiplier + constant
-
-    @classmethod
-    def _add_rows_to_output(cls, input_tags: set, dm: DataManager, td: TelemetryData,
-                            timestamp: datetime) -> tuple[list[list[str]], list[list[str]]]:
+    def _add_rows_to_output(cls, input_tags: set, dm: DataManager,
+                            td: TelemetryData) -> tuple[list[list[str]], list[list[str]]]:
         """
         Adds tags from <input_tags> and their relevant data to <output_list>
 
@@ -159,7 +142,6 @@ class DashboardHandler(UseCaseHandler):
         tag data to not yet be shown to the user
         """
 
-        data_parameters = dm.parameters
         data_tags = dm.tags
 
         include = []
@@ -167,30 +149,28 @@ class DashboardHandler(UseCaseHandler):
 
         for tag in data_tags:
 
-            tag_parameters = data_parameters[tag]
+            tag_parameters = get_tag_params(tag, dm)
             tag_description = tag_parameters.description
 
             # creating the string for the tag value
-            raw_tag_data = td.get_parameter_values(tag)
-            raw_timestamp_data = raw_tag_data[timestamp]
-            tag_data = cls._eval_param_value(tag_parameters, raw_timestamp_data)
+            raw_timestamp_data = get_tag_param_value(cls.index, tag, td)
+            tag_data = eval_param_value(tag_parameters, raw_timestamp_data)
 
             # creating the string for the tag setpoint value
             raw_tag_setpoint_value = tag_parameters.setpoint
-            tag_setpoint_value = cls._eval_param_value(
+            tag_setpoint_value = eval_param_value(
                 tag_parameters, raw_tag_setpoint_value)
 
             if type(tag_data) is bool:
                 tag_value = f'{tag_data}'
                 tag_setpoint = f'{tag_setpoint_value}'
-            elif raw_tag_setpoint_value is None:
-                unit_symbol = tag_parameters.display_units.symbol
-                tag_value = f'{tag_data} {unit_symbol}'
-                tag_setpoint = f'{tag_setpoint_value}'
             else:
                 unit_symbol = tag_parameters.display_units.symbol
                 tag_value = f'{tag_data} {unit_symbol}'
-                tag_setpoint = f'{tag_setpoint_value} {unit_symbol}'
+                if raw_tag_setpoint_value is None:
+                    tag_setpoint = f'{tag_setpoint_value}'
+                else:
+                    tag_setpoint = f'{tag_setpoint_value} {unit_symbol}'
 
             include_tag = tag in input_tags
             if include_tag:
@@ -245,7 +225,7 @@ class DashboardHandler(UseCaseHandler):
 
         # First, all the return data
         timestamp = telemetry_frame.time
-        include, remove = cls._add_rows_to_output(cls.tags, dm, telemetry_data, timestamp)
+        include, remove = cls._add_rows_to_output(cls.tags, dm, telemetry_data)
         frame_quantity = telemetry_data.num_telemetry_frames
 
         return_data = TableReturn(timestamp, include, remove, frame_quantity)
