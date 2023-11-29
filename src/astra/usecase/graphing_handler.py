6b430235d2e1c161ef9743d9cf8661e4782335ef
Alexander Mathioudakis <alex.mathioudakis@mail.utoronto.ca>
2023-11-29 14:44:00 -0500

    Improved graphing filtering algorithm efficiency.

--- src/astra/usecase/graphing_handler.py
+++ src/astra/usecase/graphing_handler.py
@@ -19,14 +19,13 @@ class GraphingData:
     :param shown_tags: A dictionary where each key is a tag, and each value is a tuple of lists.
     the first list is a list of strings representing times and the second is a list of values at
     those times for that tag. These lists must be the same length.
-    :param all_tags_values: The same format as shown_tags. However, this dictionary contains
-    contains all possible data for each tag, not just the data that is currently shown on the
-    graph. This is used to update the graph when the user changes the tags that are shown.
+    :param curr_telemetry_data: The telemetry data that was used to create the <shown_tags>. It is
+    used to correctly filter data when it is requested without accessing the database each time.
     """
 
     # The list of strings is a list of dates, formatted as <DATETIME_FORMAT>
     shown_tags: dict[Tag, tuple[list[str], list[ParameterValue]]]
-    all_tags_values: dict[Tag, tuple[list[str], list[ParameterValue]]]
+    curr_telemetry_data: TelemetryData
 
 
 @dataclass
@@ -46,6 +45,10 @@ class GraphingFilters:
 
 
 class GraphingHandler(UseCaseHandler):
+    """
+    GraphingHandler is a implementation of UseCaseHandler for the graphing tab.
+    It takes care of acquiring and formatting data needed by the graphing tab.
+    """
 
     @classmethod
     def get_data(cls, dm: DataManager, filter_args: GraphingFilters) -> GraphingData:
@@ -59,10 +62,10 @@ class GraphingHandler(UseCaseHandler):
         """
         # get the data from the database
         telemetry_data = dm.get_telemetry_data(
-            filter_args.start_time, filter_args.end_time, filter_args.shown_tags)
+            None, None, dm.tags)
 
         # initialize the graphing data, with all data from the database
-        graphing_data = cls._initialize_graphing_data(telemetry_data)
+        graphing_data = GraphingData({}, telemetry_data)
 
         # filter the graphing data by mutating it.
         cls._filter_graphing_data(graphing_data, filter_args)
@@ -157,109 +160,223 @@ class GraphingHandler(UseCaseHandler):
                 curr_tag_times.append(time.strftime(DATETIME_FORMAT))
                 cur_tag_values.append(value)
 
+    # @classmethod
+    # def _filter_graphing_data(cls, graphing_data: GraphingData,
+    #                           filter_args: GraphingFilters) -> None:
+    #     """
+    #     filter_graphing_data is a method that filters the data in <graphing_data> based on
+    #     <filter_args>. This is done by mutating <graphing_data>.
+
+    #     :param graphing_data: The data that will be filtered by mutating it.
+    #     :param filter_args: Contains all information on filters to be applied.
+    #     """
+    #     for tag in graphing_data.all_tags_values:
+
+    #         # Filter out any tags that are not in <filter_args.shown_tags> but in the graphing data.
+    #         if (tag not in filter_args.shown_tags) and (tag in graphing_data.shown_tags):
+    #             del graphing_data.shown_tags[tag]
+
+    #         # Add in any tags that need to be in <graping_data.shown_tags> but are not
+    #         elif (tag in filter_args.shown_tags) and (tag not in graphing_data.shown_tags):
+
+    #             # get the times and values for the tag
+    #             curr_tag_dates = graphing_data.all_tags_values[tag][0]
+    #             curr_tag_values = graphing_data.all_tags_values[tag][1]
+
+    #             # Add the tag and its values to shown tags.
+    #             graphing_data.shown_tags[tag] = (
+    #                 curr_tag_dates.copy(), curr_tag_values.copy())
+
+    #     # Filter out any times that are not within the start and end times
+    #     cls._filter_times(
+    #         graphing_data, filter_args.start_time, filter_args.end_time)
+
     @classmethod
     def _filter_graphing_data(cls, graphing_data: GraphingData,
                               filter_args: GraphingFilters) -> None:
         """
-        filter_graphing_data is a method that filters the data in <graphing_data> based on
-        <filter_args>. This is done by mutating <graphing_data>.
+        _filter_graphing_data is a method that filters the <graphing_data> based on <filter_args>.
+        This is done by mutating <graphing_data.shown_tags>.
 
         :param graphing_data: The data that will be filtered by mutating it.
         :param filter_args: Contains all information on filters to be applied.
         """
-        for tag in graphing_data.all_tags_values:
+        telemetry_data = graphing_data.curr_telemetry_data
 
-            # Filter out any tags that are not in <filter_args.shown_tags> but in the graphing data.
-            if (tag not in filter_args.shown_tags) and (tag in graphing_data.shown_tags):
-                del graphing_data.shown_tags[tag]
+        times_list = list(telemetry_data.timestamps())
 
-            # Add in any tags that need to be in <graping_data.shown_tags> but are not
-            elif (tag in filter_args.shown_tags) and (tag not in graphing_data.shown_tags):
+        min_index, max_index = cls._filter_times(times_list, filter_args.start_time,
+                                                 filter_args.end_time)
 
-                # get the times and values for the tag
-                curr_tag_dates = graphing_data.all_tags_values[tag][0]
-                curr_tag_values = graphing_data.all_tags_values[tag][1]
+        # Format the times to strings. Within the indices give by _filter_times.
+        formatted_times_list = [time.strftime(DATETIME_FORMAT)
+                                for time in times_list[min_index: max_index+1]]
 
-                # Add the tag and its values to shown tags.
-                graphing_data.shown_tags[tag] = (curr_tag_dates.copy(), curr_tag_values.copy())
+        # Remove all tags before looking through the filter to get only the required tags
+        graphing_data.shown_tags.clear()
 
-        # Filter out any times that are not within the start and end times
-        cls._filter_times(graphing_data, filter_args.start_time, filter_args.end_time)
+        # If the max and min index are the same, there are no values within the time_frame.
+        if min_index == max_index:
+            return None
+
+        # Loop through the <filter_args> to find which tags we must add to the data.
+        for tag in filter_args.shown_tags:
+            # Since the list of times correspond to the values, we can take the same slice of both.
+            curr_values = list(
+                graphing_data.curr_telemetry_data.get_parameter_values(tag).values())
+
+            graphing_data[tag] = (formatted_times_list,
+                                  curr_values[min_index: max_index+1])
+
+    # @classmethod
+    # def _filter_times(cls, graphing_data: GraphingData,
+    #                   start_time: datetime | None,
+    #                   end_time: datetime | None) -> None:
+    #     """
+    #     This method will filter out any times that are not within the start and end times, by
+    #     mutating the <shown_tags> parameter.
+
+    #     :param graphing: The graphing data containing the list of times that will be filtered.
+    #     The list of time must be the same length as the list of values, and sorted in chronological
+    #     order.
+    #     :param start_time: The datetime that is the earliest time that values for each tag
+    #     are from.
+    #     :param end_time: The datetime that is the latest time that values for each tag are from.
+    #     """
+    #     for tag in graphing_data.shown_tags:
+    #         times_list = graphing_data.shown_tags[tag][0]
+    #         values_list = graphing_data.shown_tags[tag][1]
+
+    #         min_index = cls._find_min_index(times_list, start_time)
+    #         max_index = cls._find_max_index(times_list, end_time)
+
+    #         # determine if there are atleast some values that are within the start and end times
+    #         if min_index == len(times_list) - 1:
+    #             del graphing_data.shown_tags[tag]
+    #         elif max_index == 0:
+    #             del graphing_data.shown_tags[tag]
+    #         else:
+    #             # there are values within the start and end times, so we keep the tag, but slice
+    #             # the list.
+    #             graphing_data.shown_tags[tag] = (times_list[min_index:max_index+1],
+    #                                              values_list[min_index:max_index+1])
 
     @classmethod
-    def _filter_times(cls, graphing_data: GraphingData,
-                      start_time: datetime | None,
-                      end_time: datetime | None) -> None:
+    def _filter_times(cls, times_list: list[datetime],
+                      start_time: datetime, end_time: datetime) -> tuple[int, int]:
         """
-        This method will filter out any times that are not within the start and end times, by
-        mutating the <shown_tags> parameter.
+        This method returns a tuple of ints representing indices that give a slice
+        of the <times_list> where all times in it are >= <start_time> and <= <end_time>.
 
-        :param graphing: The graphing data containing the list of times that will be filtered.
-        The list of time must be the same length as the list of values, and sorted in chronological
-        order.
+        :param imtes_list: The list of times in chronological order that we need the slice of, which
+        is between the <start_time> and <end_time>.
         :param start_time: The datetime that is the earliest time that values for each tag
         are from.
         :param end_time: The datetime that is the latest time that values for each tag are from.
         """
-        tags_to_delete = set()
-        for tag in graphing_data.shown_tags:
-            times_list = graphing_data.shown_tags[tag][0]
-            values_list = graphing_data.shown_tags[tag][1]
+        return (cls._find_min_index(times_list, start_time),
+                cls._find_max_index(times_list, end_time))
 
-            min_index = cls._find_min_index(times_list, start_time)
-            max_index = cls._find_max_index(times_list, end_time)
-
-            # determine if there are atleast some values that are within the start and end times
-            if min_index == len(times_list) - 1:
-                tags_to_delete.add(tag)
-            elif max_index == 0:
-                tags_to_delete.add(tag)
-            else:
-                # there are values within the start and end times, so we keep the tag, but slice
-                # the list.
-                graphing_data.shown_tags[tag] = (times_list[min_index:max_index+1],
-                                                 values_list[min_index:max_index+1])
-        for tag in tags_to_delete:
-            del graphing_data.shown_tags[tag]
-
-    @classmethod
-    def _find_min_index(cls, times_list: list[str], start_time: datetime | None) -> int:
+    def _find_min_index(times_list: list[datetime], start_time: datetime) -> int:
         """
-        This method will find the index of the first time in <times_list> that is after or equal to
-        <start_time>. If <start_time> is None, then it will return 0. (no minimum time)
+        Returns an int representing the first index in <times_list>
+        that is >= <start_time>
 
-        :param times_list: The list of times that will be searched.
-        :param start_time: The datetime that is the earliest time that values for each tag
-        are from.
-        :return: The index of the first time in <times_list> that is after <start_time>.
-        """
-        if start_time is None:
-            return 0
-        else:
-            # Iterate through the list of times, and return the index of the first time that is
-            # after or equal to <start_time>
-            for i in range(len(times_list)):
-                if datetime.strptime(times_list[i], DATETIME_FORMAT) >= start_time:
-                    return i
-            return len(times_list) - 1
+        :param times_list: A list of times that you want the min_index for
+        :param start_time: The minimum for values after the <min_index>
 
-    @classmethod
-    def _find_max_index(cls, times_list: list[str], end_time: datetime | None) -> int:
+        Precondition: times_list is sorted chronologically
         """
-        This method will find the index of the first time in <times_list> that is after or equal to
-        <start_time>. If <start_time> is None, then it will return 0. (no minimum time)
 
-        :param times_list: The list of times that will be searched.
-        :param end_time: The datetime that is the latest time that values for each tag
-        are from.
-        :return: The index of the last time in <times_list> that is before <end_time>.
+        curr_min = 0
+        curr_max = len(times_list) - 1
+
+        while curr_max >= curr_min:
+
+            index = (curr_max + curr_min)//2
+
+            if times_list[index] == start_time:
+                return index
+
+            elif times_list[index] > start_time:
+                curr_max = index - 1
+
+            elif times_list[index] < start_time:
+                curr_min = index + 1
+
+        # In the case that the loop ends, curr_min will be the index of the first time
+        # that is after <start_time>.
+        return curr_min
+
+    def _find_max_index(times_list: list[datetime], end_time: datetime) -> int:
         """
-        if end_time is None:
-            return len(times_list) - 1
-        else:
-            # Iterate through the list of times backwards and return the index of
-            # the first time that is before or equal to <end_time>.
-            for i in range(len(times_list)-1, -1, -1):
-                if datetime.strptime(times_list[i], DATETIME_FORMAT) <= end_time:
-                    return i
-            return 0
+        Returns an int representing the last index in <times_list>
+        that is <= <_tiendme>
+
+        :param times_list: A list of times that you want the max_index for
+        :param end_time: The maximum for values before the <max_index>
+
+        Precondition: times_list is sorted chronologically
+        """
+
+        curr_min = 0
+        curr_max = len(times_list) - 1
+
+        while curr_max >= curr_min:
+
+            index = (curr_max + curr_min)//2
+
+            if times_list[index] == end_time:
+                return index
+
+            elif times_list[index] > end_time:
+                curr_max = index - 1
+
+            elif times_list[index] < end_time:
+                curr_min = index + 1
+
+        # In the case that the loop ends, curr_max will be the index of the last time
+        # that is before <end_time>.
+        return curr_max
+
+    # @classmethod
+    # def _find_min_index(cls, times_list: list[str], start_time: datetime | None) -> int:
+    #     """
+    #     This method will find the index of the first time in <times_list> that is after or equal to
+    #     <start_time>. If <start_time> is None, then it will return 0. (no minimum time)
+
+    #     :param times_list: The list of times that will be searched.
+    #     :param start_time: The datetime that is the earliest time that values for each tag
+    #     are from.
+    #     :return: The index of the first time in <times_list> that is after <start_time>.
+    #     """
+    #     if start_time is None:
+    #         return 0
+    #     else:
+    #         # Iterate through the list of times, and return the index of the first time that is
+    #         # after or equal to <start_time>
+    #         for i in range(len(times_list)):
+    #             if datetime.strptime(times_list[i], DATETIME_FORMAT) >= start_time:
+    #                 return i
+    #         return len(times_list) - 1
+
+    # @classmethod
+    # def _find_max_index(cls, times_list: list[str], end_time: datetime | None) -> int:
+    #     """
+    #     This method will find the index of the first time in <times_list> that is after or equal to
+    #     <start_time>. If <start_time> is None, then it will return 0. (no minimum time)
+
+    #     :param times_list: The list of times that will be searched.
+    #     :param end_time: The datetime that is the latest time that values for each tag
+    #     are from.
+    #     :return: The index of the last time in <times_list> that is before <end_time>.
+    #     """
+    #     if end_time is None:
+    #         return len(times_list) - 1
+    #     else:
+    #         # Iterate through the list of times backwards and return the index of
+    #         # the first time that is before or equal to <end_time>.
+    #         for i in range(len(times_list)-1, -1, -1):
+    #             if datetime.strptime(times_list[i], DATETIME_FORMAT) <= end_time:
+    #                 return i
+    #         return 0
