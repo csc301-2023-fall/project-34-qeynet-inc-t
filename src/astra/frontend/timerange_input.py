8a25947136dbc4cc4c98ac6e4be03756b2001216
shape-warrior-t <bl.swt1@gmail.com>
2023-11-30 07:58:09 -0500

    Reformat

--- src/astra/frontend/timerange_input.py
+++ src/astra/frontend/timerange_input.py
@@ -22,6 +22,7 @@ def _is_short_digit_string(s: str, max_length: int) -> bool:
 
 class OperationControl(Enum):
     """A value representing whether to proceed with an operation."""
+
     CONTINUE = True
     CANCEL = False
 
@@ -31,6 +32,7 @@ type ChangeCallback = Callable[[datetime | None, datetime | None], OperationCont
 
 class TimerangeInput(Frame):
     """A frame displaying a timerange, with a button for changing the timerange."""
+
     caption: str
     start_time: datetime | None
     end_time: datetime | None
@@ -71,9 +73,9 @@ class TimerangeInput(Frame):
         """
         self.start_time = start_time
         self.end_time = end_time
-        self.timerange_label['text'] = (
-            f'{self.caption}: from {_format_time(start_time)} to {_format_time(end_time)} '
-        )
+        self.timerange_label[
+            'text'
+        ] = f'{self.caption}: from {_format_time(start_time)} to {_format_time(end_time)} '
 
     def open_set_timerange_popup(self) -> None:
         """Open a SetTimerangePopup for this TimerangeInput."""
@@ -82,6 +84,7 @@ class TimerangeInput(Frame):
 
 class SetTimerangePopup(tkinter.Toplevel):
     """A popup window to facilitate setting the timerange for a TimerangeInput."""
+
     timerange_input: TimerangeInput
     start_time_vars: list[StringVar]
     end_time_vars: list[StringVar]
@@ -107,9 +110,15 @@ class SetTimerangePopup(tkinter.Toplevel):
                     time_var = StringVar()
                     time_vars.append(time_var)
                     time_entry = Entry(time_display_frame, textvariable=time_var, width=element)
-                    time_entry.configure(validate='key', validatecommand=(time_entry.register(
-                        lambda s, max_length=element: _is_short_digit_string(s, max_length)
-                    ), '%P'))
+                    time_entry.configure(
+                        validate='key',
+                        validatecommand=(
+                            time_entry.register(
+                                lambda s, max_length=element: _is_short_digit_string(s, max_length)
+                            ),
+                            '%P',
+                        ),
+                    )
                     time_entry.pack(side=LEFT)
                     time_entries.append((time_entry, time_var, element))
                 else:
@@ -117,10 +126,15 @@ class SetTimerangePopup(tkinter.Toplevel):
             time_display_frame.grid(row=row, column=1)
         for (entry, time_var, length), (next_entry, _, _) in itertools.pairwise(time_entries):
             print((entry, time_var, length), (next_entry, None, None))
-            time_var.trace_add('write', (
-                lambda *_, entry_=entry, next_entry_=next_entry, length_=length:
-                    self.switch_entry(entry_, next_entry_, length_)
-            ))
+            time_var.trace_add(
+                'write',
+                (
+                    lambda *_,
+                    entry_=entry,
+                    next_entry_=next_entry,
+                    length_=length: self.switch_entry(entry_, next_entry_, length_)
+                ),
+            )
 
         self.set_entries_by_time(self.start_time_vars, self.timerange_input.start_time)
         self.set_entries_by_time(self.end_time_vars, self.timerange_input.end_time)
@@ -130,12 +144,20 @@ class SetTimerangePopup(tkinter.Toplevel):
         Button(
             self, text='Clear', command=lambda: self.set_entries_by_time(self.end_time_vars, None)
         ).grid(row=1, column=2, sticky='nsew')
-        Button(self, text='Fill with end time', command=lambda: self.set_entries_by_values(
-            self.start_time_vars, [time_var.get() for time_var in self.end_time_vars]
-        )).grid(row=0, column=3, sticky='nsew')
-        Button(self, text='Fill with start time', command=lambda: self.set_entries_by_values(
-            self.end_time_vars, [time_var.get() for time_var in self.start_time_vars]
-        )).grid(row=1, column=3, sticky='nsew')
+        Button(
+            self,
+            text='Fill with end time',
+            command=lambda: self.set_entries_by_values(
+                self.start_time_vars, [time_var.get() for time_var in self.end_time_vars]
+            ),
+        ).grid(row=0, column=3, sticky='nsew')
+        Button(
+            self,
+            text='Fill with start time',
+            command=lambda: self.set_entries_by_values(
+                self.end_time_vars, [time_var.get() for time_var in self.start_time_vars]
+            ),
+        ).grid(row=1, column=3, sticky='nsew')
         set_timerange_button = Button(self, text='Set time range', command=self.set_timerange)
         set_timerange_button.grid(row=2, columnspan=4, pady=(10, 0))
 
@@ -162,24 +184,34 @@ class SetTimerangePopup(tkinter.Toplevel):
         :param time_vars:
             A list of time StringVars for the corresponding set of text fields.
         """
-        values = ([format(value, '04' if i == 0 else '02') for i, value in enumerate([
-            time.year, time.month, time.day, time.hour, time.minute, time.second
-        ])] if time is not None else [''] * 6)
+        values = (
+            [
+                format(value, '04' if i == 0 else '02')
+                for i, value in enumerate(
+                    [time.year, time.month, time.day, time.hour, time.minute, time.second]
+                )
+            ]
+            if time is not None
+            else [''] * 6
+        )
         SetTimerangePopup.set_entries_by_values(time_vars, values)
 
     def set_timerange(self) -> None:
         """Actually perform the action of setting a timerange."""
         times = [None, None]
-        for i, (time_vars, time_type_capitalized, time_type_lowercase) in enumerate([
-            (self.start_time_vars, 'Start', 'start'), (self.end_time_vars, 'End', 'end')
-        ]):
+        for i, (time_vars, time_type_capitalized, time_type_lowercase) in enumerate(
+            [(self.start_time_vars, 'Start', 'start'), (self.end_time_vars, 'End', 'end')]
+        ):
             if all(not time_var.get() for time_var in time_vars):
                 times[i] = None
             elif any(not time_var.get() for time_var in time_vars):
-                messagebox.showwarning(title=f'Incomplete {time_type_lowercase} time', message=(
-                    f'{time_type_capitalized} time must either be '
-                    'completely filled or completely empty.'
-                ))
+                messagebox.showwarning(
+                    title=f'Incomplete {time_type_lowercase} time',
+                    message=(
+                        f'{time_type_capitalized} time must either be '
+                        'completely filled or completely empty.'
+                    ),
+                )
                 return
             else:
                 year, month, day, hour, minute, second = [
@@ -188,10 +220,13 @@ class SetTimerangePopup(tkinter.Toplevel):
                 try:
                     times[i] = datetime(year, month, day, hour, minute, second)
                 except ValueError:
-                    messagebox.showwarning(title=f'Invalid {time_type_lowercase} time', message=(
-                        f'{year:04}-{month:02}-{day:02} {hour:02}:{minute:02}:{second:02}'
-                        ' is not a valid time.'
-                    ))
+                    messagebox.showwarning(
+                        title=f'Invalid {time_type_lowercase} time',
+                        message=(
+                            f'{year:04}-{month:02}-{day:02} {hour:02}:{minute:02}:{second:02}'
+                            ' is not a valid time.'
+                        ),
+                    )
                     return
         start_time, end_time = times
         if self.timerange_input.onchange(start_time, end_time) is OperationControl.CONTINUE:
