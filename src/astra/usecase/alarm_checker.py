4efa1668b33ff1ed518ab1b54ab877f147d0ca70
Liam Odero <liam.odero1@gmail.com>
2023-12-05 13:35:09 -0500

    documented alarm_checker.py

--- src/astra/usecase/alarm_checker.py
+++ src/astra/usecase/alarm_checker.py
@@ -7,14 +7,18 @@ from ..data.data_manager import DataManager
 def check_alarms(dm: DataManager,
                  earliest_time: datetime) -> None:
     """
-    Goes through all possible alarms to check and, if any exists, adds them to <alarms>
-    based on their criticality
+    Goes through all possible alarm bases to check, and calls the appropriate
+    strategy to evaluate them
 
-    :param earliest_time:
-    :param dm: The manager of all data known to the program
+    This should only be called once new telemetry data is added to the system
+
+    :param dm: Contains all data known to the program
+    :param earliest_time: Details the earliest timestamp amongst newly added telemetry frames
     """
 
     alarm_bases = dm.alarm_bases
+
+    # condition variable allows us to make a notification once all strategies have completed
     cv = Condition()
     threads = []
 
@@ -22,8 +26,10 @@ def check_alarms(dm: DataManager,
         base = alarm_base.event_base
         criticality = alarm_base.criticality
 
+        # Acquiring the correct strategy for the event base then running it in a new thread
         strategy = get_strategy(base)
         new_thread = Thread(target=strategy, args=[dm, base, criticality, earliest_time, False, cv])
+
         new_thread.start()
         threads.append(new_thread)
     wait_for_children(dm, cv, threads)
@@ -31,16 +37,18 @@ def check_alarms(dm: DataManager,
 
 def wait_for_children(dm: DataManager, cv: Condition, threads: list[Thread]) -> None:
     """
-    Waits for all child threads to be completed, then notifies the alarm container that
-    all tasks are done
+    Waits for all threads in <threads> to be completed, then notifies the alarm container that
+    all alarm bases have been evaluated
 
-    :param dm: Container of the alarm container
-    :param cv: The condition variable to track child completion
+    :param dm: Contains all data known to the program
+    :param cv: The condition variable each thread in <threads> has to track thread completion
     :param threads: Contains all child threads of this process
     """
+
     thread_active = check_alive(threads)
 
     with cv:
+        # Simple loop to check if any child thread is alive and block if so
         while thread_active:
             # Note: this idea might be doable with a semaphore? My only concern is if the child
             # threads don't acquire a semaphore before this thread, then it won't work
@@ -51,10 +59,11 @@ def wait_for_children(dm: DataManager, cv: Condition, threads: list[Thread]) -> 
 
 def check_alive(threads: list[Thread]) -> bool:
     """
-    Checks if any thread in <threds> is alive and returns an appropriate boolean
+    Checks if any thread in <threads> is alive and returns an appropriate boolean
 
     :param threads: The list of threads to check
     """
+
     thread_active = False
     for thread in threads:
         if thread.is_alive():
